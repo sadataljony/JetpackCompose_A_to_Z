d38d47ffda4ac6141f5eaaf3da691be1e54a4ef5
Sadat Al-Jony <sadataljony@gmail.com>
2023-12-27 12:44:36 +0600

    implement different type of TextField

--- app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
+++ app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
@@ -3,10 +3,15 @@ package com.sadataljony.app.android.jetpackcompose_a_to_z
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,8 +20,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.sadataljony.app.android.jetpackcompose_a_to_z.ui.theme.JetpackCompose_A_to_ZTheme
@@ -31,7 +36,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SimpleTextField()
+                    Column {
+                        SimpleTextField()
+                        LabelAndPlaceHolder()
+                        TextFieldWithInputType()
+                        OutLineTextField()
+                        OutLineTextFieldWithIcon()
+                    }
                 }
             }
         }
@@ -46,6 +57,60 @@ fun SimpleTextField() {
         onValueChange = { newText ->
             text = newText
         }
+    )
+}
+
+@Composable
+fun LabelAndPlaceHolder() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Username") },
+        placeholder = { Text(text = "Enter Username") },
+    )
+}
+
+@Composable
+fun TextFieldWithInputType() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        label = { Text(text = "Mobile Number") },
+        placeholder = { Text(text = "Enter Mobile Number")},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),//KeyboardType = Text, Number, Phone, Email, Password, NumberPassword
+        onValueChange = {
+            text = it
+        }
+    )
+}
+
+@Composable
+fun OutLineTextField() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = text,
+        label = { Text(text = "Enter Your Name") },
+        onValueChange = {
+            text = it
+        }
+    )
+}
+
+@Composable
+fun OutLineTextFieldWithIcon() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    return OutlinedTextField(
+        value = text,
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "leadingIcon") },
+        trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = "trailingIcon") },
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Email address") },
+        placeholder = { Text(text = "Enter your e-mail") },
     )
 }
 
