119f0c72cc7dd2af57c53bab477465555b6f7cca
Sadat Al-Jony <sadataljony@gmail.com>
2023-12-28 12:35:29 +0600

    implement row and column

--- app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
+++ app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
@@ -3,12 +3,20 @@ package com.sadataljony.app.android.jetpackcompose_a_to_z
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.sadataljony.app.android.jetpackcompose_a_to_z.ui.theme.JetpackCompose_A_to_ZTheme
 
@@ -22,7 +30,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        SimpleRow()
+                        SimpleColumn()
+                        RowArrangement()
+                        ColumnArrangement()
+                    }
                 }
             }
         }
@@ -30,17 +43,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SimpleRow() {
+    Row {
+        Text(text = "Row Text 1", Modifier.background(Color.Red))
+        Text(text = "Row Text 2", Modifier.background(Color.White))
+        Text(text = "Row Text 3", Modifier.background(Color.Green))
+    }
+}
+
+@Composable
+fun SimpleColumn() {
+    Column {
+        Text(text = "Column Text 1", Modifier.background(Color.Red))
+        Text(text = "Column Text 2", Modifier.background(Color.White))
+        Text(text = "Column Text 3", Modifier.background(Color.Green))
+    }
+}
+
+@Composable
+fun RowArrangement(){
+    Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement  =  Arrangement.SpaceEvenly) {
+        Text(text = " Text 1")
+        Text(text = " Text 2")
+        Text(text = " Text 3")
+    }
+}
+
+@Composable
+fun ColumnArrangement(){
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment  =  Alignment.End
+    ) {
+        Text(text = "Text 1",Modifier.background(Color.Red))
+        Text(text = "Text 2",Modifier.background(Color.White))
+        Text(text = "Text 3",Modifier.background(Color.Green))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackCompose_A_to_ZTheme {
-        Greeting("Android")
+        SimpleRow()
     }
 }
