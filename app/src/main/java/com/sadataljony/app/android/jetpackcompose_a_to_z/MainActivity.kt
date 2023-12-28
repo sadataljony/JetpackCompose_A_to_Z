f50ed82200667a19f4a07527bd4f97e03d6a0c6e
Sadat Al-Jony <sadataljony@gmail.com>
2023-12-28 13:49:43 +0600

    implement image

--- app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
+++ app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
@@ -3,13 +3,32 @@ package com.sadataljony.app.android.jetpackcompose_a_to_z
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sadataljony.app.android.jetpackcompose_a_to_z.ui.theme.JetpackCompose_A_to_ZTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        SimpleImage()
+                        CircleImageView()
+                        RoundCornerImageView()
+                        ImageWithBackgroundColor()
+                        ImageWithTint()
+                        InsideFit()
+                    }
                 }
             }
         }
@@ -30,10 +56,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_android),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier
+            .width(120.dp)
+            .height(120.dp)
+    )
+}
+
+@Composable
+fun CircleImageView() {
+    Image(
+        painter = painterResource(R.drawable.ic_android),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape) // clip to the circle shape
+            .border(5.dp, Color.Gray, CircleShape)//optional
+    )
+}
+
+@Composable
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.ic_android),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, Color.Gray, RoundedCornerShape(10))
+    )
+}
+
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_android),
+        contentDescription = "",
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.DarkGray)
+            .padding(20.dp)
+    )
+}
+
+@Composable
+fun ImageWithTint() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_android),
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(Color.Red),
+        modifier = Modifier
+            .size( 200.dp)
+    )
+}
+
+@Composable
+fun InsideFit() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_android),
+        contentDescription = "",
+        modifier = Modifier
+            .size(150.dp)
+            .background(Color.LightGray),
+        contentScale = ContentScale.Inside
     )
 }
 
@@ -41,6 +131,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetpackCompose_A_to_ZTheme {
-        Greeting("Android")
+        SimpleImage()
     }
 }
