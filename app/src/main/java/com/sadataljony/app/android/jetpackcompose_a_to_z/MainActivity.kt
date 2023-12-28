d26e299817cfef34ac7e9ed548f798dfe18348f5
Sadat Al-Jony <sadataljony@gmail.com>
2023-12-28 14:59:07 +0600

    implement alert dialog

--- app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
+++ app/src/main/java/com/sadataljony/app/android/jetpackcompose_a_to_z/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.sadataljony.app.android.jetpackcompose_a_to_z
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.sadataljony.app.android.jetpackcompose_a_to_z.ui.theme.JetpackCompose_A_to_ZTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +42,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val context = LocalContext.current
+//                    AlertDialogWithTitleBodyButtons()
+                    DialogWithImage(
+                        title = "Title",
+                        body = "This is a dialog with buttons and an image.",
+                        onDismissRequest = {
+                            Toast.makeText(context, "Dismiss", Toast.LENGTH_LONG).show()
+                        },
+                        onConfirmation = {
+                            Toast.makeText(context, "Confirm", Toast.LENGTH_LONG).show()
+                        },
+                        painter = painterResource(id = R.drawable.ic_android),
+                        imageDescription = "Sample Dialog"
+                    )
                 }
             }
         }
@@ -30,17 +63,101 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun AlertDialogWithTitleBodyButtons() {
+    val context = LocalContext.current
+    AlertDialog(
+        onDismissRequest = {
+
+        },
+        title = {
+            Text(text = "Dialog Title")
+        },
+        text = {
+            Text("Here is a description text of the dialog")
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    Toast.makeText(context, "OK Button Clicked", Toast.LENGTH_LONG).show()
+                }
+            ) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    Toast.makeText(context, "Cancel Button Clicked", Toast.LENGTH_LONG).show()
+                }
+            ) {
+                Text("CANCEL")
+            }
+        }
     )
+}
+
+@Composable
+fun DialogWithImage(
+    title:String,
+    body:String,
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    painter: Painter,
+    imageDescription: String,
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        // Draw a rectangle shape with rounded corners inside the dialog
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(375.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = imageDescription,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .height(160.dp)
+                )
+                Text(
+                    text = body,
+                    modifier = Modifier.padding(16.dp),
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Dismiss")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackCompose_A_to_ZTheme {
-        Greeting("Android")
+//        AlertDialogSample()
     }
 }
